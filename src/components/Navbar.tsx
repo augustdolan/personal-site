a8176c6a772eb8bd57088b9aeaa639fac389a2c5
August Dolan <august.dolan1@gmail.com>
2023-05-09 16:08:34 -0400

    Fix build errors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,9 +5,9 @@ const Navbar = () => {
   return (
     <nav className="py-4 px-32 fixed w-full bg-emerald-950 z-10">
       <ol className="flex flex-row flex-wrap gap-4 justify-end items-center">
-        <li className="hover:text-peach"><a href="/#about">About</a></li>
-        <li className="hover:text-peach"><a href="/#projects">Projects</a></li>
-        <li className="hover:text-peach"><a href="/#contact">Contact</a></li>
+        <li className="hover:text-peach"><Link href="/#about">About</Link></li>
+        <li className="hover:text-peach"><Link href="/#projects">Projects</Link></li>
+        <li className="hover:text-peach"><Link href="/#contact">Contact</Link></li>
         <Button href="/resume" text="Resume" />
       </ol>
     </nav>
